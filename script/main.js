e73e353824af7371a13563946fadbb3fdfa593e8
Erin Zhang <zyyerin@gmail.com>
2018-03-31 19:39:15 -0700

    desktop layout

--- script/main.js
+++ script/main.js
@@ -1,11 +1,33 @@
 $(function() {
-	// $('article').hide();
 
-	$('nav a').click(function(){
+	// initialization
+	// 1. add main article
+	// 2. reset style
+	$('#primarytxt').load('text/primary_translation.html');
+		$('#secondarytxt').load('text/primary_translation.html');
+	$('#translation_btn').css({
+			'border-bottom': '1px solid black',
+			'font-weight': '600'
+			});
 
-		$('nav a').css('border-bottom', '1px solid transparent');
-		$(this).css('border-bottom', '1px solid black');
+	// primary text trigger
+	$('nav a').click(function(e){
+		e.preventDefault();
+		$("#primarytxt").hide();
+		$('nav a').css({
+			'border-bottom': '1px solid transparent',
+			'font-weight': '100'
+			});
+		$(this).css({
+			'border-bottom': '1px solid black',
+			'font-weight': '600'
+			});
 
+    	let path = $(this).attr('href');
+		$('#primarytxt').load('text/' + path +'.html');
+
+		$("html, body").stop().animate({scrollTop:0}, 500);
+		$("#primarytxt").fadeIn(1000);
 	});
 
 	$(".triTag a").click(function() {
@@ -13,6 +35,4 @@ $(function() {
 		$(".triTag a").css("background", "none");
 		$(this).css("background", "red");
 	});
-
-
 });
