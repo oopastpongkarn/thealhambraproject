31d07d049598156db3bc7723c23da385fc4976f6
Erin Zhang <zyyerin@gmail.com>
2018-04-22 15:56:27 -0700

    tri hover effect and load content on hover

--- script/main.js
+++ script/main.js
@@ -1,5 +1,31 @@
 $(function() {
 
+	$('img[src$=".svg"]').each(function(){
+
+     let $img = $(this);
+     let imgID = $img.attr('id');
+     let imgClass = $img.attr('class');
+     let imgURL = $img.attr('src');
+
+     jQuery.get(imgURL, function(data) {
+         // Get the SVG tag, ignore the rest
+         let $svg = jQuery(data).find('svg');
+         // Add replaced image's ID to the new SVG
+         if(typeof imgID !== 'undefined') {
+             $svg = $svg.attr('id', imgID);
+         }
+         // Add replaced image's classes to the new SVG
+         if(typeof imgClass !== 'undefined') {
+             $svg = $svg.attr('class', imgClass+' replaced-svg');
+         }
+         // Remove any invalid XML tags as per http://validator.w3.org
+         $svg = $svg.removeAttr('xmlns:a');
+         // Replace image with new SVG
+         $img.replaceWith($svg);
+     }, 'xml');
+
+ 	});
+
 	// initialization
 	// 1. add main article
 	// 2. reset style
@@ -12,8 +38,6 @@ $(function() {
 	// primary text trigger
 	$('nav a').click(function(e){
 		e.preventDefault();
-		// $("#primarytxt").hide();
-		$("#primarytxt").css("opacity", 0);
 		$('nav a').css({
 			'border-bottom': '1px solid transparent',
 			'font-weight': '100'
@@ -25,12 +49,15 @@ $(function() {
 
     	let path = $(this).attr('href');
 		$('#primarytxt').load('text/' + path +'.html');
-		$("#primarytxt").animate({opacity:1}, 1000);
 	});
 
-	// $(".triTag a").click(function() {
-	// 	$( "#secondarytxt" ).load( "/text/"+ $(this).attr('id') +".txt" );
-	// 	$(".triTag a").css("background", "none");
-	// 	$(this).css("background", "red");
-	// });
+	$(".svg").click(function() {
+		e.preventDefault();
+		console.log("bang");
+		$("#secondarytxt").load( "/links/"+ $(this).attr('href') +".html" );
+		// $("polygon", this).css("fill", "red !important");
+	});
+
+
+
 });
